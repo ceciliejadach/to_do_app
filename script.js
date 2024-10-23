7cabb5bd2ffca8a692a2f84d19661905ee7fba19
Cecilie <cepe0001@stud.kea.dk>
2024-10-23 10:22:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,132 @@
+//variabler for elementer
+const toDoList = document.getElementById("to_do_list_container");
+const inputTask = document.getElementById("task");
+
+const addButton = document.querySelector("button");
+const h1Done = document.getElementById("done");
+const doneList = document.getElementById("done_list_container");
+const removeAllTasksBtn = document.getElementById("remove_all");
+
+const taskArrayFromLocalstorage = localStorage.getItem("taskArray");
+
+//ugens dag bliver vist
+const weekday = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("demo").innerHTML = day;
+
+//Lav array (det skal være tomt fra starten)
+let taskArray = [];
+
+//Gem taskArray i localstorage
+if (taskArrayFromLocalstorage === null) {
+  taskArray = [];
+} else {
+  taskArray = JSON.parse(taskArrayFromLocalstorage);
+}
+
+//controller
+init();
+
+function init() {
+  updateTaskView();
+}
+
+//Ved klik af tilføj knappen tilføjes task til array
+addButton.addEventListener("click", addTask);
+
+//Tilføj task til to-do liste ved at trykke enter på keyboard
+inputTask.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+//Tilføj task til array
+function addTask() {
+  let task = inputTask.value; //gemmer opgaven der er blevet skrevet i inputfeltet, så jeg kan pushe opgaven (task) til sidst i array'et
+
+  if (inputTask.value === "") {
+    alert("Hov du skal da lige have skrevet en opgave ind!"); //hvis man ikke har skrevet noget i inputfelt, kommer der en alert
+  } else if (taskArray.includes(task)) {
+    alert("Den opgave er allerede tilføjet"); //man kan ikke skrive den samme task to gange
+  } else {
+    taskArray.push(task); //tilføjer task til arrayét
+  }
+
+  updateTaskView(); //kalder funktionen updateTaskView, så man kan se opgaverne
+  inputTask.value = ""; //inputfeltet ryddes
+}
+
+//opdater visning af array
+function updateTaskView() {
+  toDoList.innerHTML = "";
+
+  taskArray.forEach((task, id) => {
+    let li = document.createElement("li");
+
+    li.innerHTML = `<span class="taskname" data-id="${id}"> ${task}</span> <span class="delete" data-id="${id}">&#128465</span>`; //tasks får specifikt ID
+    toDoList.appendChild(li); //tilføj li element
+  });
+
+  //Ved klik på taskname marker den som done
+  document.querySelectorAll(".taskname").forEach((task) => {
+    task.addEventListener("click", markAsDone);
+  });
+
+  //Slet task ved klik på skraldespanden med klassen delete
+  document.querySelectorAll(".delete").forEach((deleteButton) => {
+    deleteButton.addEventListener("click", deleteTask);
+  });
+
+  updateLocalStorage(); //Opdater localStorage efter ændringer
+}
+
+// Markér en opgave som færdig og flyt den til done-listen
+function markAsDone(event) {
+  let completedTask = event.target.innerText;
+  let taskId = event.target.dataset.id;
+  let li = document.createElement("li");
+
+  // li.innerHTML = `<span class="strike">${completedTask}</span> <span class="delete">&#128465</span>`;
+  li.innerHTML = completedTask;
+  li.classList.add("strike"); //tilføj klassen strike til task, når den er markeret som done
+  console.log("task er done");
+
+  doneList.appendChild(li); //Opret et nyt <li> element i done-listen
+  li.addEventListener("click", markAsNotDone); // Tilføj eventlistener til task for at kunne flytte den tilbage til to-do-listen
+  taskArray.splice(taskId, 1); //Fjern opgaven fra taskArray
+
+  updateTaskView(); //opdater visningen af to-do-listen
+  updateLocalStorage();
+}
+
+//Flyt task tilbage til to-do-listen
+function markAsNotDone(event) {
+  let taskToMoveBack = event.target.innerText;
+  taskArray.push(taskToMoveBack); //skub task tilbage til taskArray
+  event.target.remove(); //Fjern opgaven fra done-listen
+  console.log("Task bliver flyttet tilbage til to-do listen");
+
+  updateTaskView(); //Opdater to-do-listen
+  updateLocalStorage(); //Opdater localStorage efter ændringer
+}
+
+//Funktion til at slette en opgave
+function deleteTask(event) {
+  let taskId = event.target.dataset.id; // Hent id fra data-id
+  taskArray.splice(taskId, 1); // Fjern opgaven fra taskArray
+
+  updateTaskView(); //Opdater visningen af to-do-listen
+  updateLocalStorage(); //Opdater localStorage efter ændringer
+}
+
+//Knap til at fjerne alt indhold i done listen
+removeAllTasksBtn.addEventListener("click", function (e) {
+  doneList.innerHTML = "";
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("taskArray", JSON.stringify(taskArray));
+}
